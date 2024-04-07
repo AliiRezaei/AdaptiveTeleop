6c2c66d998c617c1d563c7f1447c60a43e65c964
aliirezaei <alii.rezzaei@gmail.com>
2024-04-07 14:04:07 -0700

    comment plot path

--- RobotPlotter.m
+++ RobotPlotter.m
@@ -13,13 +13,13 @@ classdef RobotPlotter
         end
 
         function line_hand = get_line_hand(obj, current_axes, q, varargin)
-            joint_pos = [obj.robot.l1 * cos(q(1)); obj.robot.l2 * sin(q(1))] + obj.base_pos;
-            ee_pos = [obj.robot.l1 * cos(q(1) + q(2)); obj.robot.l2 * sin(q(1) + q(2))] + obj.base_pos;
+            joint_pos = [obj.robot.l1 * cos(q(1)); obj.robot.l1 * sin(q(1))] + obj.base_pos;
+            ee_pos = [obj.robot.l2 * cos(q(1) + q(2)); obj.robot.l2 * sin(q(1) + q(2))] + obj.base_pos;
 
             X = [obj.base_pos(1), joint_pos(1), ee_pos(1)];
             Y = [obj.base_pos(2), joint_pos(2), ee_pos(2)];
 
-            % links position in 3D space :
+            % links position in 2D space :
             Link1X = [X(1),X(2)];
             Link1Y = [Y(1),Y(2)];
             
@@ -30,11 +30,11 @@ classdef RobotPlotter
             line2 = line(current_axes, Link2X, Link2Y, varargin{:}); % link2
 
             line_hand = {line1, line2};
-            
-            hold on
-            % plot trajectory that followed by end-effector
-            plot(X(end), Y(end), 'r.')
-            hold off
+
+            % hold on
+            % % plot trajectory that followed by end-effector
+            % plot(X(end), Y(end), 'r.')
+
         end
 
     end
