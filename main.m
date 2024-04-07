692e1830d60889f9ee4ac2bc74334e88fb9a5163
aliirezaei <alii.rezzaei@gmail.com>
2024-04-07 15:28:15 -0700

    main sections

--- main.m
+++ main.m
@@ -2,44 +2,65 @@ clc
 clear
 close all
 
+%% Declare Master and Slave Robots
+
+% master robot :
 masterRobot.robot = RobotClass();
 masterRobot.base_pos = [0; 0];
 masterRobot.plotter = RobotPlotter(masterRobot.robot, masterRobot.base_pos);
 
+% slave robot :
 slaveRobot.robot = RobotClass();
 slaveRobot.base_pos = [0; 0];
 slaveRobot.plotter = RobotPlotter(slaveRobot.robot, slaveRobot.base_pos);
 
+%% GUI Object and Time Vars
 
+% gui object :
 app = RobotApp(masterRobot, slaveRobot);
 
+% time vars :
 SimTime = 50;
 dt = 0.1;
 t = (0:dt:SimTime)';
 nt = numel(t);
 
+%% Master and Slave Initial States
+
+% master robot joints pod and vel :
 qm  = randn(2, 1);
 dqm = zeros(2, 1);
 
+% slave robot joints pod and vel :
 qs  = randn(2, 1);
 dqs = zeros(2, 1);
 
+%% Allocate Memory for Loop Params
+
+% master robot control signal :
+Um = zeros(nt, 1);
+
+% slave robot control signal :
+Us = zeros(nt, 1);
+
+%% Simulate Robots
+
 for k = 1:nt
     % master side :
-    Um = app.master_controller(t(k), qm, dqm);
+    Um(k, 1) = app.master_controller(t(k), qm, dqm);
     Mm  = masterRobot.robot.get_mass_matrix(qm);
     Cm  = masterRobot.robot.get_coriolis(qm, dqm);
     Gm  = masterRobot.robot.get_graviry(qm);
-    ddqm = Mm \ (Um - Cm * dqm - Gm);
+    ddqm = Mm \ (Um(k, 1) - Cm * dqm - Gm);
     dqm  = ddqm * dt + dqm;
     qm   = dqm  * dt + qm;
 
     % slave side :
-    Us = app.slave_controller(qs, dqs, qm, dqm, ddqm);
+    Us(k, 1) = app.slave_controller(qs, dqs, qm, dqm, ddqm);
     Ms  = slaveRobot.robot.get_mass_matrix(qs);
     Cs  = slaveRobot.robot.get_coriolis(qs, dqs);
     Gs  = slaveRobot.robot.get_graviry(qs);
-    ddqs = Ms \ (Us - Cs * dqs - Gs);
+    ddqs = Ms \ (Us(k, 1) - Cs * dqs - Gs);
     dqs  = ddqs * dt + dqs;
     qs   = dqs  * dt + qs;
 
@@ -52,5 +73,7 @@ for k = 1:nt
         delete(line_handle_s{2});
     end
 end
+
+%% Plots and Results
 
 
