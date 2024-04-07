749c71a5f9b6032fb47016737324a2b1c039b150
aliirezaei <alii.rezzaei@gmail.com>
2024-04-07 10:05:02 -0700

    get mass matrix

---
+++ RobotClass.m
@@ -0,0 +1,37 @@
+classdef RobotClass
+
+    properties
+        m1;
+        m2;
+        l1;
+        l2;
+        lc1;
+        lc2;
+        I1;
+        I2;
+    end
+
+    methods
+
+        % Constructor :
+        function obj = RobotClass()
+            
+        end
+
+        function M = get_mass_matrix(obj, q)
+
+            p1 = obj.m1 * obj.lc1^2 + obj.m2 * obj.l1^2 + obj.I1;
+            p2 = obj.m2 * obj.lc2^2 + obj.I2;
+            p3 = obj.m2 * obj.l1 * obj.lc2;
+
+            M = zeros(2, 2);
+            M(1, 1) = p1 + p2 + 2 * p3 * cos(q(2));
+            M(1, 2) = p2 + p3 * cos(q(2));
+            M(2, 1) = M(1, 2);
+            M(2, 2) = p2;
+        end
+
+    end
+
+
+end % end of RobotClass
