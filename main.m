52509c3bcfd14347136a5aa476727fe015d658db
aliirezaei <alii.rezzaei@gmail.com>
2024-04-07 14:23:02 -0700

    test master controller

--- main.m
+++ main.m
@@ -13,6 +13,28 @@ slaveRobot.plotter = RobotPlotter(slaveRobot.robot, slaveRobot_base_pos);
 
 app = RobotApp(masterRobot, slaveRobot);
 
-qm = randn(2, 1);
-qs = randn(2, 1);
-app.draw_robots(qm, qs);
+SimTime = 10;
+dt = 0.1;
+tSpan = (0:dt:SimTime)';
+nt = numel(tSpan);
+
+qm  = randn(2, 1);
+dqm = zeros(2, 1);
+
+for i = 1:nt
+    um = app.master_controller(qm, dqm);
+    M  = masterRobot.robot.get_mass_matrix(qm);
+    C  = masterRobot.robot.get_coriolis(qm, dqm);
+    G  = masterRobot.robot.get_graviry(qm);
+    ddqm = M \ (um - C * dqm - G);
+    dqm  = ddqm * dt + dqm;
+    qm   = dqm  * dt + qm;
+    line_handle = app.draw_robots(qm, qm);
+    drawnow expose
+    if i ~= nt
+        delete(line_handle{1});
+        delete(line_handle{2});
+    end
+end
+
+
