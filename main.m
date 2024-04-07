fdb944ce065ca8ac1c99248792aac94e7c82e8f9
aliirezaei <alii.rezzaei@gmail.com>
2024-04-07 14:57:41 -0700

    test master side

--- main.m
+++ main.m
@@ -15,25 +15,34 @@ app = RobotApp(masterRobot, slaveRobot);
 
 SimTime = 10;
 dt = 0.1;
-tSpan = (0:dt:SimTime)';
-nt = numel(tSpan);
+t = (0:dt:SimTime)';
+nt = numel(t);
 
 qm  = randn(2, 1);
 dqm = zeros(2, 1);
 
-for i = 1:nt
-    um = app.master_controller(qm, dqm);
-    M  = masterRobot.robot.get_mass_matrix(qm);
-    C  = masterRobot.robot.get_coriolis(qm, dqm);
-    G  = masterRobot.robot.get_graviry(qm);
-    ddqm = M \ (um - C * dqm - G);
+qs  = randn(2, 1);
+dqs = zeros(2, 1);
+
+for k = 1:nt
+    % master side :
+    Um = app.master_controller(t(k), qm, dqm);
+    Mm  = masterRobot.robot.get_mass_matrix(qm);
+    Cm  = masterRobot.robot.get_coriolis(qm, dqm);
+    Gm  = masterRobot.robot.get_graviry(qm);
+    ddqm = Mm \ (Um - Cm * dqm - Gm);
     dqm  = ddqm * dt + dqm;
     qm   = dqm  * dt + qm;
-    line_handle = app.draw_robots(qm, qm);
-    drawnow expose
-    if i ~= nt
-        delete(line_handle{1});
-        delete(line_handle{2});
+
+    % slave side :
+
+    [line_handle_m, line_handle_s] = app.draw_robots(qm, qm);
+    drawnow
+    if k ~= nt
+        delete(line_handle_m{1});
+        delete(line_handle_m{2});
+        delete(line_handle_s{1});
+        delete(line_handle_s{2});
     end
 end
 
