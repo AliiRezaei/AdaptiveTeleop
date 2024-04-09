63667bb8dec8c7999f3840ce44e77cbcd7a22a8b
aliirezaei <alii.rezzaei@gmail.com>
2024-04-09 11:02:17 -0700

    adapt in both side

--- demo1.m
+++ demo1.m
@@ -50,10 +50,15 @@ Us = zeros(nt, 2);
 
 %% Simulate Robots
 
-a_hat = zeros(4, 1);
+a_hat_m = zeros(4, 1);
+a_hat_s = zeros(4, 1);
+% a_hat = [2.1555
+%    -3.2388
+%    -9.6484
+%     8.1978];
 for k = 1:nt
     % master side :
-    [Um(k, :), a_hat] = app.master_controller(t(k), qm, dqm, a_hat);
+    [Um(k, :), a_hat_m] = app.master_controller(t(k), qm, dqm, a_hat_m);
     Mm  = masterRobot.robot.get_mass_matrix(qm);
     Cm  = masterRobot.robot.get_coriolis(qm, dqm);
     Gm  = masterRobot.robot.get_graviry(qm);
@@ -62,7 +67,7 @@ for k = 1:nt
     qm   = dqm  * dt + qm;
 
     % slave side :
-    Us(k, :) = app.slave_controller(qs, dqs, qm, dqm, ddqm);
+    [Us(k, :), a_hat_s] = app.slave_controller(qs, dqs, qm, dqm, ddqm, a_hat_s);
     Ms  = slaveRobot.robot.get_mass_matrix(qs);
     Cs  = slaveRobot.robot.get_coriolis(qs, dqs);
     Gs  = slaveRobot.robot.get_graviry(qs);
@@ -70,15 +75,16 @@ for k = 1:nt
     dqs  = ddqs * dt + dqs;
     qs   = dqs  * dt + qs;
 
-    % [line_handle_m, line_handle_s] = app.draw_robots(t(k), qm, qs);
-    % % app.real_time_draw()
-    % drawnow
-    % if k ~= nt
-    %     delete(line_handle_m{1});
-    %     delete(line_handle_m{2});
-    %     delete(line_handle_s{1});
-    %     delete(line_handle_s{2});
-    % end
+    if rem(k, 1000) == 0
+        [line_handle_m, line_handle_s] = app.draw_robots(t(k), qm, qs);
+        drawnow
+        if k ~= nt
+            delete(line_handle_m{1});
+            delete(line_handle_m{2});
+            delete(line_handle_s{1});
+            delete(line_handle_s{2});
+        end
+    end
 end
 
 %% Plots and Results
